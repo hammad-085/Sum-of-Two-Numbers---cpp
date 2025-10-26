d0a8e73565ef2c3d0916daa8b3c0926aee8b6fbb
Hammad Haider <233998966+hammad-085@users.noreply.github.com>
2025-10-26 08:36:17 +0500

    Implement sum of two numbers in sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+                                          // Sum of Two Numbers
+int main ()
+{
+  int num1, num2, sum;
+
+  cout << "Enter First Number: ";
+  cin >> num1;
+
+  cout << "Enter Second Number: ";
+  cin >> num2;
+
+  sum = num1 + num2;
+
+  cout << "The Sum is: " << sum;
+
+  return 0;
+}
